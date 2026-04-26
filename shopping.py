7f60362210c7b8d1a9b09d44f9201fbda944ab36
MUSTAFE MAHDI <168520315+EngMustaphe@users.noreply.github.com>
2026-04-26 09:50:19 +0300

    Refactor shopping.py for clarity and functionality

--- shopping.py
+++ shopping.py
@@ -10,13 +10,28 @@
 # else:
 #     print("nor enought money")
 
+# item_price=100
+# user_money=80
+# discount=20
+# price=item_price
+# money=user_money
+# if item_price >=100:
+#     print("buy successful")
+# elif item_price>=80:
+#     print("buy with discount")
+# else:
+#     print("nor enought money")
+
 item_price = 100
 user_money = 80
 discount = 20
 
-total = user_money + discount
-
-if total >= item_price:
-    print("Buy successful with discount")
+if user_money>=item_price:
+    print("your bu succesfull")
+if user_money + item_price:
+    print("your bu succefful with diccount")
 else:
-    print("Not enough money")
+    print("your money not enough")
+
+
+
